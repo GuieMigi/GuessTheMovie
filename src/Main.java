ae05dc87ef59d3ad38b260bfea04f306fb4c4d2e
GuieMigi <florinchiriac2004@yahoo.com>
2018-07-29 15:41:27 +0300

    Stored the movie list inside an ArrayList

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        
+
+        File file = new File("movies.txt");
+        ArrayList<String> movieList = new ArrayList<>();
+        boolean hasWon = false;
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                movieList.add(scanner.nextLine());
+            }
+            System.out.println(movieList.size());
+        } catch (FileNotFoundException exception) {
+            System.out.println("The file is not available.");
+        } catch (IndexOutOfBoundsException exception) {
+            System.out.println("There is no more room available inside the array.");
+        }
     }
 }
